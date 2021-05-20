e97a3b984aedb28a4bfcedec03c422fee8a0501d
alexgsalg <alexgsalg@gmail.com>
2021-05-19 22:04:28 -0300

    Resolving merge conflicts

--- src/App.js
+++ src/App.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-import './App.scss';
-import './assets/sass/main.scss';
-import Navbar from './components/Navbar';
-
-function App() {
-  return (
-    <div className="app">
-      <Navbar />
-    </div>
-  );
-}
-
-export default App;
-=======
 import './App.scss';
 import './assets/sass/main.scss';
 import Navbar from './components/Navbar';
@@ -31,4 +16,3 @@ function App() {
 }
 
 export default App;
->>>>>>> 2fc649f9975072db29f716fa372786a93bc426c6
