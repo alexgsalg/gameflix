6f77e795f8da601a2630a4ff5b3bccffd53f1450
alexgsalg <alexgsalgado@gmail.com>
2021-05-24 09:40:16 -0300

    new readme photo favorite toggle

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,16 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 
 import { ReactComponent as Star } from '../assets/icons/Star.svg'
 
 
 function Card({ item }) {
+  const [favorite, SetFavorite] = useState(false)
   return (
     <div className="game">
-      <Star className="game__favorite" />
+      {/* While api doesn't have a favorite field */}
+      <Star className={favorite || item.id === 1 ? 'game__favorite favorite' : 'game__favorite'} onClick={() => { SetFavorite(!favorite) }} />
+      {/* <Star className={favorite ? 'game__favorite favorite' : 'game__favorite'} onClick={() => { SetFavorite(!favorite) }} /> */}
       <img src={item.thumbnail} alt="Game name" className="game__img" />
       <div className="game__overlay"></div>
       <p className="game__title">{item.title}</p>
