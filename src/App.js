00e47fa8e59188e20b742917bb7d6021ef801029
alexgsalg <alexgsalgado@gmail.com>
2021-06-22 20:03:50 -0300

    Timed Banner, Filtered games and LoadMore

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.scss';
 import './assets/sass/main.scss';
 
@@ -12,30 +12,59 @@ function App() {
   const games = gamelist.games;
 
   const [countdown, setCountdown] = useState(true);
-  // const [gameArray, setGameArray] = useState(games);
+  const [filteredGames, setFilteredGames] = useState(games.filter(item => item.id <= 10));
+  const [gameGenre, setGameGenre] = useState("All");
+  const [loadMore, setLoadMore] = useState(12);
 
-  const filteredGames = games.filter(item => item.id <= 10);
-  console.log(filteredGames)
 
+  const handleLoadMore = () => {
+    setLoadMore(loadMore + 12)
+  }
+
+
+  useEffect(() => {
+    if (gameGenre === "All") {
+      setFilteredGames(games.filter(item => item.id <= loadMore))
+    } else {
+      setFilteredGames(games.filter(item => item.genre === gameGenre).slice(0, loadMore))
+    }
+
+  }, [gameGenre, loadMore])
+
+  let genreArray = [];
+  games.map(item => {
+    if (!genreArray.includes(item.genre)) {
+      genreArray.push(item.genre)
+    }
+    return genreArray
+  })
 
   return (
     <div className="app">
       <Navbar />
       <main className="main">
         <div className="wrapper">
-          <Sidebar games={games} />
+          <Sidebar games={games}
+            genreArray={genreArray}
+            setGameGenre={setGameGenre}
+            gameGenre={gameGenre}
+            setLoadMore={setLoadMore}
+          />
           <div className="content">
             {countdown ? <Banner setCountdown={setCountdown} /> : null}
             <div className="gamegrid">
-              {filteredGames.map(item => {
+              {filteredGames.map((item) => {
                 return (
-                  <Card item={item} />
+                  <Card item={item} key={item.id} />
                 )
               })}
             </div>
+            <div className={`button-container ${filteredGames.length < 12 ? 'disabled' : ''}`}>
+              <button className="button" onClick={() => { handleLoadMore() }}>Load More</button>
+            </div>
 
-            Content
-                </div>
+
+          </div>
         </div>
       </main>
     </div >
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,9 @@
-import React, { useState } from 'react'
-
-
-import { ReactComponent as Star } from '../assets/icons/Star.svg'
-
+import React from 'react'
 
 function Card({ item }) {
-  const [favorite, SetFavorite] = useState(false)
+
   return (
     <div className="game">
-      {/* While api doesn't have a favorite field */}
-      <Star className={favorite || item.id === 1 ? 'game__favorite favorite' : 'game__favorite'} onClick={() => { SetFavorite(!favorite) }} />
-      {/* <Star className={favorite ? 'game__favorite favorite' : 'game__favorite'} onClick={() => { SetFavorite(!favorite) }} /> */}
       <img src={item.thumbnail} alt="Game name" className="game__img" />
       <div className="game__overlay"></div>
       <p className="game__title">{item.title}</p>
